d639b5385c5fb233ee9e526a19a647d13fea4386
Hyeon Yujin <gusdbwls9772@gmail.com>
2021-01-28 21:57:12 +0900

    Add Date & introduce medeoduck

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 (function() {
   const barElem = document.querySelector('.progress-bar');
   let maxScrollValue;
+  const date = new Date();
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const dayElem = document.querySelector('.day');
+  const monthElem = document.querySelector('.month');
+  const dateElem = document.querySelector('.mon-date');
+  const yearElem = document.querySelector('.year');
 
   function resizeHandler() {
     maxScrollValue = document.body.offsetHeight - window.innerHeight;
@@ -14,4 +21,10 @@
 
   window.addEventListener('resize', resizeHandler);
   resizeHandler();
+
+  // date
+  dayElem.innerHTML = days[date.getDay()] + ",";
+  monthElem.innerHTML = months[date.getMonth()];
+  dateElem.innerHTML = date.getDate() + ",";
+  yearElem.innerHTML = date.getFullYear();
 })();
